aac29bddf068d894d6b086ed13f57080f10357fe
Aqua <86239876+Aqua-123@users.noreply.github.com>
2023-11-16 23:08:11 +0530

    Initial Commit

---
+++ model_load.py
@@ -0,0 +1,44 @@
+import gradio as gr
+import torch
+import torchvision.transforms as transforms
+from PIL import Image
+from torchvision import models, transforms
+import torch.nn as nn
+
+# Load the trained model
+model = models.resnet50(pretrained=True)
+num_ftrs = model.fc.in_features
+model.fc = nn.Linear(num_ftrs, 3)  # Assuming 3 classes
+model.load_state_dict(torch.load("NSFW_Model.pth", map_location="cuda:0"))
+model = model.to("cuda:0")
+model.eval()
+
+# Define the image transformations
+transform = transforms.Compose(
+    [
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+    ]
+)
+
+
+# Function to predict the image
+def predict_image(image):
+    image = Image.fromarray(image.astype("uint8"), "RGB")
+    image = transform(image).unsqueeze(0)
+    image = image.to("cuda:0")
+
+    with torch.no_grad():
+        outputs = model(image)
+        _, predicted = torch.max(outputs, 1)
+        predicted_class = predicted.item()
+        
+    return f"Predicted class: {predicted_class}"
+
+
+# Create a Gradio interface
+iface = gr.Interface(fn=predict_image, inputs=gr.inputs.Image(), outputs="text")
+
+# Launch the interface
+iface.launch(server_name="0.0.0.0",share=True)
